e390e45b4d5dbb91754e04e49283fb4e6cf586f7
liuyuan <liuyuan@liuyuandeMacBook-Air.local>
2026-04-19 00:48:38 +0800

    移动位置

--- crates/zom-core/src/command/catalog/specs/mod.rs
+++ crates/zom-core/src/command/catalog/specs/mod.rs
@@ -1,17 +1,14 @@
 mod editor;
-mod workspace_actions;
-mod workspace_file_tree;
-mod workspace_panels;
-mod workspace_tab;
+mod workspace;
 
 use crate::command::catalog::CommandSpec;
 
 pub(super) fn collect_specs() -> Vec<CommandSpec> {
     let mut specs = Vec::new();
     specs.extend_from_slice(editor::SPECS);
-    specs.extend_from_slice(workspace_panels::SPECS);
-    specs.extend_from_slice(workspace_actions::SPECS);
-    specs.extend_from_slice(workspace_file_tree::SPECS);
-    specs.extend_from_slice(workspace_tab::SPECS);
+    specs.extend_from_slice(workspace::panels::SPECS);
+    specs.extend_from_slice(workspace::actions::SPECS);
+    specs.extend_from_slice(workspace::file_tree::SPECS);
+    specs.extend_from_slice(workspace::tab::SPECS);
     specs
 }
---
+++ crates/zom-core/src/command/catalog/specs/workspace/mod.rs
@@ -0,0 +1,4 @@
+pub mod actions;
+pub mod file_tree;
+pub mod panels;
+pub mod tab;
